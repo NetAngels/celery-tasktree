f12587dca207e3281aea1ca74b590e3cfb1539f3
Ivan Petukhov <ivan@netangels.ru>
2011-06-08 14:55:39 +0600

    Changes in setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,14 @@ setup(
     author='NetAngels team',
     author_email='info@netangels.ru',
     url='https://github.com/NetAngels/celery-tasktree',
+    long_description = open('README.rst').read().decode('utf-8'),
+    license = 'BSD License',
     py_modules=['celery_tasktree'],
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
 )
